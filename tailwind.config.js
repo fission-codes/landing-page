12d09d0e3486c918fba81e9149139574981701db
ryanbetts <depatchedmode@gmail.com>
2023-04-19 10:55:32 -0700

    header, footer and default page spacing tweaks

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,21 +10,14 @@ module.exports = {
 
     extend: {
       backgroundImage: {
-        "hero-dark":
+        "hero":
           "linear-gradient(358.98deg, rgba(100, 70, 250, 0) 10.03%, rgba(100, 70, 250, 0.2) 99.18%);",
-        "hero-light":
-          "linear-gradient(358.98deg, rgba(100, 70, 250, 0) 10.03%, rgba(100, 70, 250, 0.2) 99.18%)",
-        "text-bg-dark": "linear-gradient(92.57deg, #FF5274 0%, #6446FA 99.99%)",
-        "newsletter-dark":
-          "linear-gradient(134.46deg, rgba(255, 82, 116, 0.4) 0%, rgba(100, 70, 250, 0.4) 100%)",
-        "newsletter-light":
-          "linear-gradient(92.57deg, #FF5274 0%, #6446FA 99.99%)",
         "overlay-menu":
           "linear-gradient(358.98deg, rgba(255, 82, 116, 0) 10.03%, rgba(100, 70, 250, 0.2) 99.18%)",
       },
       colors: {
-        ...kit.dasherizeObjectKeys(kit.colors),
-        neutral: {
+        // ...kit.dasherizeObjectKeys(kit.colors),
+        newneutral: {
           0: "#FBFBFC",
           50: "#F5F5F9",
           100: "#EEEEF5",
@@ -36,18 +29,6 @@ module.exports = {
           700: "#1F223D",
           800: "#0E1225",
           900: "#070B16",
-        },
-        yellow: {
-          50: "#FEFCE8",
-          100: "#FFE8A4",
-          200: "#FFC967",
-          300: "#FFB238",
-          400: "#EB971B",
-          500: "#CC7B0B",
-          600: "#A96103",
-          700: "#7B4401",
-          800: "#5E3403",
-          900: "#4A2A07",
         },
         /*
           Named `newpurple` and `newpink` to avoid breaking Fission Kit imports
@@ -93,17 +74,17 @@ module.exports = {
         "body-lg": ["22px", { lineHeight: "29px" }],
         "body-xl": ["26px", { lineHeight: "34px" }],
         "body-2xl": ["31px", { lineHeight: "40px" }],
-        xs: ["13px", { lineHeight: "15px" }],
-        sm: ["16px", { lineHeight: "24px" }],
-        base: ["18px", { lineHeight: "21px" }],
-        lg: ["24px", { lineHeight: "30px" }],
-        xl: ["32px", { lineHeight: "38px" }],
-        "2xl": ["43px", { lineHeight: "52px" }],
-        "3xl": ["57px", { lineHeight: "69px" }],
+        xs: ["14px", { lineHeight: "18px" }],
+        sm: ["18px", { lineHeight: "23.4px" }],
+        base: ["20px", { lineHeight: "26px" }],
+        lg: ["24px", { lineHeight: "36px" }],
+        xl: ["32px", { lineHeight: "41px" }],
+        "2xl": ["48px", { lineHeight: "57.6px" }],
+        "3xl": ["60px", { lineHeight: "78px" }],
       },
       maxWidth: {
-        "3xl": "775px",
-        "5xl": "936px",
+        "3xl": "948px",
+        "5xl": "1352px",
       },
       zIndex: {
         max: "1000",
