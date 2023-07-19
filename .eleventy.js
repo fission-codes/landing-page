ae2ea2fe9d9d2e1a2fcf240709c3feb47a93ddd1
avivash <av@andrewvivash.com>
2023-07-19 13:41:06 -0700

    Feat: script to download and save audio files locally

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const cleanCSS = require("clean-css");
+const https = require("https");
 const fs = require("fs");
 const pluginRSS = require("@11ty/eleventy-plugin-rss");
 const Image = require("@11ty/eleventy-img");
@@ -56,6 +57,22 @@ const imageShortcode = async (src, alt, widths) => {
     },
   };
 }
+
+// Download audio files and save them locally
+const downloadFile = async (fileName, url, dest, cb) => {
+  fs.mkdir(dest, { recursive: true }, (err) => {
+    if (err) throw err;
+    const file = fs.createWriteStream(`${dest}/${fileName}`, { flags: 'w' });
+
+    https.get(url, (response) => {
+      response.pipe(file);
+
+      file.on("finish", () => {
+        file.close(cb);
+      });
+    });
+  });
+};
 
 module.exports = function(config) {
   // Minify HTML
@@ -192,7 +209,7 @@ module.exports = function(config) {
         console.error(err);
       });
 
-    collection.forEach(async (post) => {
+    collection.forEach(async (post, index) => {
       post.url = stripDomain(post.url);
       post.primary_author.url = stripDomain(post.primary_author.url);
       post.tags.map((tag) => (tag.url = stripDomain(tag.url)));
@@ -205,6 +222,23 @@ module.exports = function(config) {
       post.thumbnail_image = thumbnail.url;
       post.thumbnail_image_width = thumbnail.width;
       post.thumbnail_image_height = thumbnail.height;
+
+      // Parse audio player src and save it relatively
+      if (post.html.includes("<audio src=")) {
+        let src;
+        const audioSrcs = [];
+        const rex = /<audio[^>]+src="?([^"\s]+)"?\s* /g;
+        while ((src = rex.exec(post.html))) {
+          audioSrcs.push(src[1]);
+        }
+
+        const callback = () => console.log('audio file downloaded')
+        audioSrcs.forEach(audioSrc => {
+          const urlParts = audioSrc.split('/')
+          const fileName = urlParts[urlParts.length - 1]
+          downloadFile(fileName, audioSrc, "dist/relativeLocalAudio", callback);
+        })
+      }
 
       // Convert publish date into a Date object
       post.published_at = new Date(post.published_at);
